ff9e2430e82ff8d4511a5c3715425f6b9f4db0d8
Cristian Monsalvo <cnmonsalvo@gmail.com>
2024-02-18 17:56:04 -0300

    Modif Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 import Home from "./components/Home";
 import Productos from "./components/Productos";
 import Aromas from "./components/Aromas";
-import Nosotros from "./components/Nosotros";
+import Nosotras from "./components/Nosotras";
 import Contacto from "./components/Contacto";
 import "./scss/index.scss";
 
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/productos" element={<Productos />} />
         <Route path="/aromas" element={<Aromas />} />
-        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/nosotros" element={<Nosotras />} />
         <Route path="/contacto" element={<Contacto />} />
       </Routes>
     </BrowserRouter>
