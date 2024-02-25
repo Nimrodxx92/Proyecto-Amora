dcb487c5a7acfa0d23999515896ed215d4d2945e
BryhanS <bryhansalazarpacsi@gmail.comm>
2024-02-24 22:39:28 -0500

    aromas banner

--- src/components/Aromas.jsx
+++ src/components/Aromas.jsx
@@ -5,13 +5,30 @@ import AromasDescription from "./AromasDescription";
 const Aromas = () => {
   const [arrayAromas, setArrayAromas] = useState([...aromasDescription.aromas]);
 
-  console.log(arrayAromas);
   return (
-    <section className="section__container">
-      {arrayAromas.map((info, id) => (
-        <AromasDescription item={info} key={id} />
-      ))}
-    </section>
+    <>
+      <section className="banner__container">
+        <div className="banner__container__left">
+          <div className="banner__container__left__text">
+            <h1>¿Es tu primera compra?</h1>
+            <p>
+              Recibí asesoramiento personalizado en tu compra para poder
+              descubrir y elegir la fragancia que mejor se ajusta a vos y a tus
+              necesidades.
+            </p>
+          </div>
+          <button>Hacé click acá</button>
+        </div>
+        <div>
+          <img src="../src/assets/aromas/banner.png" alt="banner de aromas" />
+        </div>
+      </section>
+      <section className="section__container">
+        {arrayAromas.map((info, id) => (
+          <AromasDescription item={info} key={id} />
+        ))}
+      </section>
+    </>
   );
 };
 
