f8cbb6f01991d065525707fecf681df8dfe99071
Cristian Monsalvo <cnmonsalvo@gmail.com>
2024-03-09 14:36:19 -0300

    Img contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,7 +1,7 @@
 import { Ig, Email, Whats, Fb } from "../assets/icons/icons";
-import img1 from "../../public/contacto/contacto-1";
-import img2 from "../../public/contacto/contacto-2";
-import img3 from "../../public/contacto/contacto-3";
+import img1 from "../assets/contacto/contacto-1.jpg";
+import img2 from "../assets/contacto/contacto-2.jpg";
+import img3 from "../assets/contacto/contacto-3.jpg";
 
 const Contacto = () => {
   return (
